c6d723a4893554f2a09b05df5420bb4246450df5
unknown <andre.curval66@gmail.com>
2025-01-24 09:02:34 +0100

    first commit

---
+++ src/main/java/ch/manajos/manajos/ManajosApplication.java
@@ -0,0 +1,13 @@
+package ch.manajos.manajos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManajosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/manajos/manajos/ManajosApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.manajos.manajos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
